e1223702b0998638c3c99433b20904bb2f5f48de
jack-fetz <jfetzer@hcrhs.org>
2019-02-27 11:03:06 -0500

    Test of push from ATOM

--- 3pin_distance/3pin_distance.ino
+++ 3pin_distance/3pin_distance.ino
@@ -1,7 +1,7 @@
-   
+
   Serial.begin (9600);
 }
- 
+
 void loop() {
   pinMode(distPin, OUTPUT);
   digitalWrite(distPin, LOW);
@@ -13,12 +13,12 @@ void loop() {
   delayMicroseconds(10);
   pinMode(distPin, INPUT);
   duration = pulseIn(distPin, HIGH);
- 
+
   inches = (duration/2) / 74;
-  
+
   Serial.print(inches);
   Serial.print("in, ");
   Serial.println();
-  
+
   delay(25 );
 }
