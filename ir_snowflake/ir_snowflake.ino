6d450d7a3e8ea041cb9c351433a16bf65a74ffee
Grace Meiman <grace.meiman.142425@gmail.com>
2019-03-21 14:21:37 -0400

    Update ir_snowflake.ino
    Assigned int to each pin output (the line spacing is still incorrect).

--- ir_snowflake/ir_snowflake.ino
+++ ir_snowflake/ir_snowflake.ino
@@ -6,23 +6,28 @@ int oringeffect;
 int aringeffect;
 int inout;
 
+//Current arrangement: center = 7, inner ring = 1,2,3,4,5,6, outer ring = 8,9,10,11,12,0, IR remote = 13
+int c = 7;
+int i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5, i6 = 6;
+int o1 = 8, o2 = 9, o3 = 10, o4 = 11, o5 = 12, o6 = 0;
+
 IRrecv irrecv(receiver);
 decode_results results;
 
 void setup() {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(o6, OUTPUT);
+  pinMode(i1, OUTPUT);
+  pinMode(i2, OUTPUT);
+  pinMode(i3, OUTPUT);
+  pinMode(i4, OUTPUT);
+  pinMode(i5, OUTPUT);
+  pinMode(i6, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(o1, OUTPUT);
+  pinMode(o2, OUTPUT);
+  pinMode(o3, OUTPUT);
+  pinMode(o4, OUTPUT);
+  pinMode(o5, OUTPUT);
   pinMode(13, INPUT);
   iringeffect = 0;
   oringeffect = 0;
@@ -31,128 +36,128 @@ void setup() {
 }
 
 void inouteffect() {
-  digitalWrite(7, HIGH);
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
+  digitalWrite(c, HIGH);
+  digitalWrite(i1, LOW);
+  digitalWrite(i2, LOW);
+  digitalWrite(i3, LOW);
+  digitalWrite(i4, LOW);
+  digitalWrite(i5, LOW);
+  digitalWrite(i6, LOW);
   delay(350);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
-  digitalWrite(11, HIGH);
-  digitalWrite(12, HIGH);
-  digitalWrite(0, HIGH);
-  digitalWrite(7, LOW);
+  digitalWrite(o1, HIGH);
+  digitalWrite(o2, HIGH);
+  digitalWrite(o3, HIGH);
+  digitalWrite(o4, HIGH);
+  digitalWrite(o5, HIGH);
+  digitalWrite(o6, HIGH);
+  digitalWrite(c, LOW);
   delay(350);
-  digitalWrite(1, HIGH);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(0, LOW);
+  digitalWrite(i1, HIGH);
+  digitalWrite(i2, HIGH);
+  digitalWrite(i3, HIGH);
+  digitalWrite(i4, HIGH);
+  digitalWrite(i5, HIGH);
+  digitalWrite(i6, HIGH);
+  digitalWrite(o1, LOW);
+  digitalWrite(o2, LOW);
+  digitalWrite(o3, LOW);
+  digitalWrite(o4, LOW);
+  digitalWrite(o5, LOW);
+  digitalWrite(o6, LOW);
   delay(350);
 }
 
 void oeffect() {
-        digitalWrite(1, HIGH);
+        digitalWrite(i1, HIGH);
         delay(50);
-        digitalWrite(6, LOW);
+        digitalWrite(i6, LOW);
         delay(50);
-        digitalWrite(2, HIGH);
+        digitalWrite(i2, HIGH);
         delay(50);
-        digitalWrite(1, LOW);
+        digitalWrite(i1, LOW);
         delay(50);
-        digitalWrite(3, HIGH);
+        digitalWrite(i3, HIGH);
         delay(50);
-        digitalWrite(2, LOW);
+        digitalWrite(i2, LOW);
         delay(50);
-        digitalWrite(4, HIGH);
+        digitalWrite(i4, HIGH);
         delay(50);
-        digitalWrite(3, LOW);
+        digitalWrite(i3, LOW);
         delay(50);
-        digitalWrite(5, HIGH);
+        digitalWrite(i5, HIGH);
         delay(50);
-        digitalWrite(4, LOW);
+        digitalWrite(i4, LOW);
         delay(50);
-        digitalWrite(6, HIGH);
+        digitalWrite(i6, HIGH);
         delay(50);
-        digitalWrite(5, LOW);
+        digitalWrite(i5, LOW);
         delay(50);
 }
 
 void ieffect() {
-        digitalWrite(8, HIGH);
+        digitalWrite(o1, HIGH);
         delay(50);
-        digitalWrite(0, LOW);
+        digitalWrite(o6, LOW);
         delay(50);
-        digitalWrite(9, HIGH);
+        digitalWrite(o2, HIGH);
         delay(50);
-        digitalWrite(8, LOW);
+        digitalWrite(o1, LOW);
         delay(50);
-        digitalWrite(10, HIGH);
+        digitalWrite(o3, HIGH);
         delay(50);
-        digitalWrite(9, LOW);
+        digitalWrite(o2, LOW);
         delay(50);
-        digitalWrite(11, HIGH);
+        digitalWrite(o4, HIGH);
         delay(50);
-        digitalWrite(10, LOW);
+        digitalWrite(o3, LOW);
         delay(50);
-        digitalWrite(12, HIGH);
+        digitalWrite(o5, HIGH);
         delay(50);
-        digitalWrite(11, LOW);
+        digitalWrite(o4, LOW);
         delay(50);
-        digitalWrite(0, HIGH);
+        digitalWrite(o6, HIGH);
         delay(50);
-        digitalWrite(12, LOW);
+        digitalWrite(o5, LOW);
         delay(50);
 }
 
 void aeffect() {
-        digitalWrite(7, HIGH);
-        digitalWrite(8, HIGH);
-        digitalWrite(1, HIGH);
+        digitalWrite(c, HIGH);
+        digitalWrite(o1, HIGH);
+        digitalWrite(i1, HIGH);
         delay(50);
-        digitalWrite(0, LOW);
-        digitalWrite(6, LOW);
+        digitalWrite(o6, LOW);
+        digitalWrite(i6, LOW);
         delay(50);
-        digitalWrite(9, HIGH);
-        digitalWrite(2, HIGH);
+        digitalWrite(o2, HIGH);
+        digitalWrite(i2, HIGH);
         delay(50);
-        digitalWrite(8, LOW);
-        digitalWrite(1, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(i1, LOW);
         delay(50);
-        digitalWrite(10, HIGH);
-        digitalWrite(3, HIGH);
+        digitalWrite(o3, HIGH);
+        digitalWrite(i3, HIGH);
         delay(50);
-        digitalWrite(9, LOW);
-        digitalWrite(2, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(i2, LOW);
         delay(50);
-        digitalWrite(11, HIGH);
-        digitalWrite(4, HIGH);
+        digitalWrite(o4, HIGH);
+        digitalWrite(i4, HIGH);
         delay(50);
-        digitalWrite(10, LOW);
-        digitalWrite(3, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(i3, LOW);
         delay(50);
-        digitalWrite(12, HIGH);
-        digitalWrite(5, HIGH);
+        digitalWrite(o5, HIGH);
+        digitalWrite(i5, HIGH);
         delay(50);
-        digitalWrite(11, LOW);
-        digitalWrite(4, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(i4, LOW);
         delay(50);
-        digitalWrite(0, HIGH);
-        digitalWrite(6, HIGH);
+        digitalWrite(o6, HIGH);
+        digitalWrite(i6, HIGH);
         delay(50);
-        digitalWrite(12, LOW);
-        digitalWrite(5, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(i5, LOW);
         delay(50);
 }
 
@@ -178,55 +183,55 @@ void loop() {
      aeffect();
   }
   if (irrecv.decode(&results)) {
-    if (results.value == 0xFF18E7 && digitalRead(8) == LOW) { // #2 Inner Ring Static
-        digitalWrite(8, HIGH);
-        digitalWrite(9, HIGH);
-        digitalWrite(10, HIGH);
-        digitalWrite(11, HIGH);
-        digitalWrite(12, HIGH);
-        digitalWrite(0, HIGH);
+    if (results.value == 0xFF18E7 && digitalRead(o1) == LOW) { // #2 Inner Ring Static
+        digitalWrite(o1, HIGH);
+        digitalWrite(o2, HIGH);
+        digitalWrite(o3, HIGH);
+        digitalWrite(o4, HIGH);
+        digitalWrite(o5, HIGH);
+        digitalWrite(o6, HIGH);
         results.value = 0x000000;
         delay(500);
     }
-    else if (results.value == 0xFF18E7 && digitalRead(8) == HIGH) {
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+    else if (results.value == 0xFF18E7 && digitalRead(o1) == HIGH) {
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         iringeffect = 0;
         results.value = 0x000000;
         delay(500);
     }
-    else if (results.value == 0xFF7A85 && digitalRead(6) == LOW && digitalRead(2) == LOW) { // #3 Outer Ring Static
-        digitalWrite(1, HIGH);
-        digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, HIGH);
-        digitalWrite(6, HIGH);
+    else if (results.value == 0xFF7A85 && digitalRead(i6) == LOW && digitalRead(i2) == LOW) { // #3 Outer Ring Static
+        digitalWrite(i1, HIGH);
+        digitalWrite(i2, HIGH);
+        digitalWrite(i3, HIGH);
+        digitalWrite(i4, HIGH);
+        digitalWrite(i5, HIGH);
+        digitalWrite(i6, HIGH);
         results.value = 0x000000;
         delay(250);
     }
-    else if (results.value == 0xFF7A85 && digitalRead(6) == HIGH && digitalRead(2) == HIGH) {
-        digitalWrite(1, LOW);
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
+    else if (results.value == 0xFF7A85 && digitalRead(i6) == HIGH && digitalRead(i2) == HIGH) {
+        digitalWrite(i1, LOW);
+        digitalWrite(i2, LOW);
+        digitalWrite(i3, LOW);
+        digitalWrite(i4, LOW);
+        digitalWrite(i5, LOW);
+        digitalWrite(i6, LOW);
         oringeffect = 0;
         results.value = 0x000000;
         delay(250);
     }
-    else if (results.value == 0xFF30CF && digitalRead(7) == LOW) { // #1 Center Static
-        digitalWrite(7, HIGH);
+    else if (results.value == 0xFF30CF && digitalRead(c) == LOW) { // #1 Center Static
+        digitalWrite(c, HIGH);
         results.value = 0x000000;
         delay(250);
     }
-    else if (results.value == 0xFF30CF && digitalRead(7) == HIGH) {
-        digitalWrite(7, LOW);
+    else if (results.value == 0xFF30CF && digitalRead(c) == HIGH) {
+        digitalWrite(c, LOW);
         results.value = 0x000000;
         delay(250);
     }
@@ -237,12 +242,12 @@ void loop() {
     }
     else if (results.value == 0xFF10EF && iringeffect == 1) {
         iringeffect = 0;
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         results.value = 0x000000;
         delay(250);
     }
@@ -253,18 +258,18 @@ void loop() {
     }
     else if (results.value == 0xFF38C7 && oringeffect == 1) {
         oringeffect = 0;
-        digitalWrite(1, LOW);
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+        digitalWrite(i1, LOW);
+        digitalWrite(i2, LOW);
+        digitalWrite(i3, LOW);
+        digitalWrite(i4, LOW);
+        digitalWrite(i5, LOW);
+        digitalWrite(i6, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         results.value = 0x000000;
         delay(250);
     }
@@ -274,19 +279,19 @@ void loop() {
     }
     else if (results.value == 0xFF5AA5 && aringeffect == 1) {
         aringeffect = 0;
-        digitalWrite(1, LOW);
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+        digitalWrite(i1, LOW);
+        digitalWrite(i2, LOW);
+        digitalWrite(i3, LOW);
+        digitalWrite(i4, LOW);
+        digitalWrite(i5, LOW);
+        digitalWrite(i6, LOW);
+        digitalWrite(c, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         delay(250);
     }
     else if (results.value == 0xFF6897 && inout == 0) { // #0 InOut Effect
@@ -296,36 +301,36 @@ void loop() {
     }
     else if (results.value == 0xFF6897 && inout == 1) {
         inout = 0;
-        digitalWrite(1, LOW);
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+        digitalWrite(i1, LOW);
+        digitalWrite(i2, LOW);
+        digitalWrite(i3, LOW);
+        digitalWrite(i4, LOW);
+        digitalWrite(i5, LOW);
+        digitalWrite(i6, LOW);
+        digitalWrite(c, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         results.value = 0x000000;
         delay(250);
     }
-    else if (results.value == 0xFFA25D && digitalRead(1) == HIGH  || digitalRead(2) == HIGH || digitalRead(3) == HIGH || digitalRead(4) == HIGH || digitalRead(5) == HIGH || digitalRead(6) == HIGH || digitalRead(0) == HIGH || digitalRead(8) == HIGH || digitalRead(7) == HIGH) { // All Off
-        digitalWrite(1, LOW);
-        digitalWrite(2, LOW);
-        digitalWrite(3, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(6, LOW);
-        digitalWrite(7, LOW);
-        digitalWrite(8, LOW);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(11, LOW);
-        digitalWrite(12, LOW);
-        digitalWrite(0, LOW);
+    else if (results.value == 0xFFA25D && digitalRead(i1) == HIGH  || digitalRead(i2) == HIGH || digitalRead(i3) == HIGH || digitalRead(i4) == HIGH || digitalRead(i5) == HIGH || digitalRead(i6) == HIGH || digitalRead(o6) == HIGH || digitalRead(o1) == HIGH || digitalRead(c) == HIGH) { // All Off
+        digitalWrite(i1, LOW);
+        digitalWrite(i2, LOW);
+        digitalWrite(i3, LOW);
+        digitalWrite(i4, LOW);
+        digitalWrite(i5, LOW);
+        digitalWrite(i6, LOW);
+        digitalWrite(c, LOW);
+        digitalWrite(o1, LOW);
+        digitalWrite(o2, LOW);
+        digitalWrite(o3, LOW);
+        digitalWrite(o4, LOW);
+        digitalWrite(o5, LOW);
+        digitalWrite(o6, LOW);
         iringeffect = 0;
         oringeffect = 0;
         aringeffect = 0;
@@ -334,19 +339,19 @@ void loop() {
         delay(500);
     }
     else if (results.value == 0xFFA25D) { // All On
-        digitalWrite(1, HIGH);
-        digitalWrite(2, HIGH);
-        digitalWrite(3, HIGH);
-        digitalWrite(4, HIGH);
-        digitalWrite(5, HIGH);
-        digitalWrite(6, HIGH);
-        digitalWrite(7, HIGH);
-        digitalWrite(8, HIGH);
-        digitalWrite(9, HIGH);
-        digitalWrite(10, HIGH);
-        digitalWrite(11, HIGH);
-        digitalWrite(12, HIGH);
-        digitalWrite(0, HIGH);
+        digitalWrite(i1, HIGH);
+        digitalWrite(i2, HIGH);
+        digitalWrite(i3, HIGH);
+        digitalWrite(i4, HIGH);
+        digitalWrite(i5, HIGH);
+        digitalWrite(i6, HIGH);
+        digitalWrite(c, HIGH);
+        digitalWrite(o1, HIGH);
+        digitalWrite(o2, HIGH);
+        digitalWrite(o3, HIGH);
+        digitalWrite(o4, HIGH);
+        digitalWrite(o5, HIGH);
+        digitalWrite(o6, HIGH);
         results.value = 0x000000;
     }
     results.value = 0x000000;
